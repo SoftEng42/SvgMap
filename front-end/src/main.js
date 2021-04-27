28664436c733d327f2a5a4b6536cfa0db02050e0
SoftEng42 <coolfor42@gmail.com>
2021-04-27 21:34:25 +0300

    fix

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -11,8 +11,9 @@ Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 if (Vue.config.productionTip) {
   // Vue.axios.defaults.baseURL = 'https://test.memlist.se/api/v3';
+  Vue.axios.defaults.baseURL = 'http://46.17.248.20/:8000/api/';
 } else {
-  Vue.axios.defaults.baseURL = 'http://localhost:8000/api/';
+  Vue.axios.defaults.baseURL = 'http://46.17.248.20/:8000/api/';
 }
 new Vue({
   render: h => h(App)
